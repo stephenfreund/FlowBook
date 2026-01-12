083d0afd5f8a4cc81ad31fe28e09e6c56ddda10c
Stephen Freund <freund@cs.williams.edu>
2026-01-12 17:46:23 -0500

    scripts

--- scripts/run-base.sh
+++ scripts/run-base.sh
@@ -2,4 +2,4 @@
 
 # env FERRET_PROFILE_CHECKPOINT=1 \
 # env FERRET_PROFILE_DIFF=1 \
-data_ferret_slurm $*  -- execute_all
+flowbook $*  -- execute_base
--- scripts/run-flowbook.sh
+++ scripts/run-flowbook.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-env FERRET_PROFILE_CHECKPOINT=1 \
-env FERRET_PROFILE_DIFF=1 \
-data_ferret_slurm $*  -- execute_sdc
+# env FERRET_PROFILE_CHECKPOINT=1 \
+# env FERRET_PROFILE_DIFF=1 \
+flowbook $*  -- execute
